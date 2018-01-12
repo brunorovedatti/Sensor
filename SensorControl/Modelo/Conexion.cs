6587357b7de35f4770f883de99a1d102ffcbd2e4
brunorovedatti <bruno.rovedatti@gmail.com>
2018-01-12 14:41:57 -0300

    Agregue las tablas que faltaban en la capa MODELO

---
+++ SensorControl/Modelo/Conexion.cs
@@ -0,0 +1,18 @@
+﻿namespace Modelo
+{
+    public class Conexion
+    {
+        public int Id_Conexion { get; set; }
+        public string Nombre_Conexion { get; set; }
+        public bool Estado_Conexion { get; set; }
+
+        public Conexion() { }
+
+        public Conexion(int pId_Conexion, string pNombre_Conexion, bool pEstado_Conexion)
+        {
+            this.Id_Conexion = pId_Conexion;
+            this.Nombre_Conexion = pNombre_Conexion;
+            this.Estado_Conexion = pEstado_Conexion;
+        }
+    }
+}
